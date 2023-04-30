f9b8496460247a55373317b615183da74dd3b090
IgorTeleychuk <teleychuk.i@yandex.r>
2023-04-30 11:07:31 +0700

    feature_comments: fix reviewer's questions 3

--- ewm-service/src/main/java/ru/practicum/main_service/exception/TimeLessTwoHourException.java
+++ ewm-service/src/main/java/ru/practicum/main_service/exception/TimeLessTwoHourException.java
@@ -1,6 +1,6 @@
 package ru.practicum.main_service.exception;
 
-public class TimeLessTwoHourException extends RuntimeException{
+public class TimeLessTwoHourException extends RuntimeException {
     public TimeLessTwoHourException(String message) {
         super(message);
     }
